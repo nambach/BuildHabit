1b8a9ef22c3e7c113decdb5224744fb01af8071e
Unknown <nambach2@Gmail.com>
2018-06-04 23:01:38 +0700

    Fix the add-habit API input type [NamBM]
    Change the rowkey into date

--- src/main/java/io/nambm/buildhabit/controller/HabitController.java
+++ src/main/java/io/nambm/buildhabit/controller/HabitController.java
@@ -4,7 +4,6 @@ import io.nambm.buildhabit.model.HabitModel;
 import io.swagger.annotations.*;
 import org.springframework.http.ResponseEntity;
 
-import java.util.Date;
 import java.util.List;
 
 @Api(description = "Habit API")
@@ -15,6 +14,7 @@ public interface HabitController {
             @ApiImplicitParam(name = "username", defaultValue = "nambm", value = "Username"),
             @ApiImplicitParam(name = "title", value = "Title", defaultValue = ""),
             @ApiImplicitParam(name = "description", value = "Description", defaultValue = ""),
+            @ApiImplicitParam(name = "schedule", value = "Schedule", defaultValue = "{\"from\":{\"hour\":6,\"minute\":0},\"to\":{\"hour\":7,\"minute\":0},\"repetition\":\"weekly\",\"times\":[\"mon\",\"tue\",\"sat\"]}"),
             @ApiImplicitParam(name = "tags", value = "Tags", defaultValue = "[]"),
             @ApiImplicitParam(name = "startTime", value = "Start Time", defaultValue = ""),
             @ApiImplicitParam(name = "endTime", value = "End Time", defaultValue = "")
@@ -24,7 +24,7 @@ public interface HabitController {
             @ApiResponse(code = 409, message = "User has already existed")
     })
     ResponseEntity add(String username, String title, String description,
-                       List<String> tags, String startTime, String endTime);
+                       String schedule, String tags, String startTime, String endTime);
 
     @ApiOperation("Load all habits")
     @ApiResponse(code = 200, message = "Found")
--- src/main/java/io/nambm/buildhabit/controller/impl/HabitControllerImpl.java
+++ src/main/java/io/nambm/buildhabit/controller/impl/HabitControllerImpl.java
@@ -2,7 +2,9 @@ package io.nambm.buildhabit.controller.impl;
 
 import io.nambm.buildhabit.controller.HabitController;
 import io.nambm.buildhabit.model.HabitModel;
+import io.nambm.buildhabit.model.submodel.Schedule;
 import io.nambm.buildhabit.service.HabitService;
+import io.nambm.buildhabit.util.JsonUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,15 +28,20 @@ public class HabitControllerImpl implements HabitController {
     public ResponseEntity add(@RequestParam String username,
                               @RequestParam String title,
                               @RequestParam String description,
-                              @RequestParam List<String> tags,
+                              @RequestParam String schedule,
+                              @RequestParam String tags,
                               @RequestParam String startTime,
                               @RequestParam String endTime) {
         HabitModel habitModel = new HabitModel();
         habitModel.setUsername(username);
-        habitModel.setId(new Date().toString());
+        habitModel.setId(new Date().getTime() + "");
+
         habitModel.setTitle(title);
         habitModel.setDescription(description);
-        habitModel.setTags(tags);
+
+        habitModel.setSchedule(Schedule.from(schedule));
+        habitModel.setTags(JsonUtils.getArray(tags, String.class));
+
         habitModel.setStartTime(Long.parseLong(startTime));
         habitModel.setEndTime(Long.parseLong(endTime));
 

cfc3206aa544bf0855554b3be20d2c44afd09820
NAMBM <nambach2@Gmail.com>
2018-07-12 17:14:56 +0700

    Implement JWT Security Configuration [NamBM]

--- src/main/java/io/nambm/buildhabit/business/UserBusiness.java
+++ src/main/java/io/nambm/buildhabit/business/UserBusiness.java
@@ -3,4 +3,6 @@ package io.nambm.buildhabit.business;
 import io.nambm.buildhabit.model.user.UserModel;
 
 public interface UserBusiness extends GenericBusiness<UserModel> {
+
+    UserModel get(String username);
 }
